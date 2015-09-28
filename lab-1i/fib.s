b27c052a37d0dcdddc324145464cb23effd04ec7
Gary-Lai <gary821018@yahoo.com.tw>
2015-09-28 13:39:47 +0800

    Iterative using r3,r4,r5
    r5 is the latest value,r5=r3+r4
    then move r3 to r4, and r5 to r3
    try to form a loop

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,33 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
+
+
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r3,#1
+	mov r4,#-1
+	mov r5,#0
+	subs r0,r0,#0
+	bge Loop
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	
+Loop:
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	add r5,r3,r4
+	mov r4,r3
+	mov r3,r5
+	subs r0,r0,#1	
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	bge Loop
 
-	@ R0 = R5 + R0 (update flags)
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r0,r5     @return value
 
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3, r4, r5, pc}	
+
 
 	.size fibonacci, .-fibonacci
+
 	.end
