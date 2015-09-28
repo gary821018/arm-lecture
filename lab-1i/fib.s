240b6e328129c6e5973d9d4a192b876a1f3f7e78
Gary-Lai <gary821018@yahoo.com.tw>
2015-09-28 21:50:09 +0800

    Add comment,and compare with c code
    Try to put some c code in comment,
    make it easy to link the meanings between c and assembly.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,27 +13,30 @@ fibonacci:
 
 	push {r3, r4, r5, lr}
 
-	mov r3,#1
-	mov r4,#-1
-	mov r5,#0
-	subs r0,r0,#0
-	bge Loop
+	mov r3,#1	@result=1
+	mov r4,#-1	@previos=-1
+	mov r5,#0	@sum=0	
+	cmp r0,#0	@if(x<=0)
+	ble except
 
 	
 Loop:
 
-	add r5,r3,r4
-	mov r4,r3
-	mov r3,r5
-	subs r0,r0,#1	
-
+	add r5,r3,r4	@sum=previos+result
+	mov r4,r3	@previous=result
+	mov r3,r5	@result=sum
+	subs r0,r0,#1	@x--
+	
+	cmp r0,#0	@while (x>=0)
 	bge Loop
 
+	mov r0,r5  @retun value
+	pop {r3,r4,r5,pc}
 
-	mov r0,r5     @return value
+except:
+	mov r0,#0	@return value 0
+	pop {r3,r4,r5,pc}
 
-
-	pop {r3, r4, r5, pc}	
 
 
 	.size fibonacci, .-fibonacci
